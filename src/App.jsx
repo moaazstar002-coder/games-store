be286573a9fed88888706c9e138ea9ad71b9b23f
moaaz hussain <moaazstar002@gmail.com>
2025-12-10 21:50:17 +0200

    update:add lazy loading using {lazy,suspense}

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-import Home from "./pages/Home";
-import GameDetails from "./pages/gameDetails";
-import AllGames from "./pages/AllGames";
-import WishList from "./pages/wishList";
-import Cart from "./pages/cart";
-import About from "./pages/about";
-import NotFound from "./pages/404_NotFound";
 import { Toaster } from 'sonner';
+import Loader from "./components/Loader";
+
+// Lazy load pages
+const Home = lazy(() => import("./pages/Home"));
+const GameDetails = lazy(() => import("./pages/gameDetails"));
+const AllGames = lazy(() => import("./pages/AllGames"));
+const WishList = lazy(() => import("./pages/wishList"));
+const Cart = lazy(() => import("./pages/cart"));
+const About = lazy(() => import("./pages/about"));
+const NotFound = lazy(() => import("./pages/404_NotFound"));
 
 function App() {
   return (
@@ -17,16 +20,17 @@ function App() {
     <Toaster position="top-right" richColors />
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game/:id" element={<GameDetails />} />
-         <Route path="/games" element={<AllGames />} />
-        <Route path="/WishList" element={<WishList />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game/:id" element={<GameDetails />} />
+           <Route path="/games" element={<AllGames />} />
+          <Route path="/WishList" element={<WishList />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </BrowserRouter>
     </>
