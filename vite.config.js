7a04b5c3add68b8330bb02bb30db2c85f6d359df
moaaz hussain <moaazstar002@gmail.com>
2025-12-17 20:35:01 +0200

    Add hooks with correct naming

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux'],
+          query: ['@tanstack/react-query'],
+        }
+      }
+    }
   },
-  
+  server: {
+    port: 3000,
+    open: true
+  }
 })
