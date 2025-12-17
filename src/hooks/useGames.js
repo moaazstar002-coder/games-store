3dc5c3ca2030929fac273591f236ac4e0580d68d
moaaz hussain <moaazstar002@gmail.com>
2025-12-17 21:57:54 +0200

    fix bugs in useGames

--- src/hooks/useGames.js
+++ src/hooks/useGames.js
@@ -1,20 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-const key = import.meta.env.VITE_RAWG_API_KEY || "2c014c5b22214e628eecac2b366c6441 ";
-// const key = import.meta.env.VITE_RAWG_API_KEY;
-
+const key =
+  import.meta.env.VITE_RAWG_API_KEY || "2c014c5b22214e628eecac2b366c6441";
 
 export function useGames({ genre, search, page, ordering } = {}) {
   return useQuery({
-    queryKey: ["games", genre || "all", search || "", page || 1, ordering || "default"],
+    queryKey: [
+      "games",
+      genre || "all",
+      search || "",
+      page || 1,
+      ordering || "default",
+    ],
     queryFn: async () => {
       const params = {
         key: key,
         page_size: 20,
         page: page || 1,
-        
       };
-console.log("API Key:", key);
+      console.log("API Key:", key);
       if (genre) params.genres = genre;
       if (search) params.search = search;
       if (ordering) params.ordering = ordering;
