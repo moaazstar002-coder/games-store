f61238b7e8c8aaa98a074e5613808df1e1a5d038
moaaz hussain <moaazstar002@gmail.com>
2025-12-17 22:02:07 +0200

    fix bugs in hooks

--- src/hooks/useGameDetails.js
+++ src/hooks/useGameDetails.js
@@ -1,5 +1,6 @@
 
-const key = import.meta.env.VITE_RAWG_API_KEY;
+const key =
+  import.meta.env.VITE_RAWG_API_KEY || "2c014c5b22214e628eecac2b366c6441";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useParams } from "react-router-dom";
--- src/hooks/useGenres.js
+++ src/hooks/useGenres.js
@@ -1,19 +1,19 @@
-
-const key = import.meta.env.VITE_RAWG_API_KEY;
+const key =
+  import.meta.env.VITE_RAWG_API_KEY || "2c014c5b22214e628eecac2b366c6441";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export function useGenres() {
   return useQuery({
     queryKey: ["genres"],
-    queryFn: async()=>{
-        const res = await axios.get("https://api.rawg.io/api/genres", {
+    queryFn: async () => {
+      const res = await axios.get("https://api.rawg.io/api/genres", {
         params: {
-            key: key,
+          key: key,
         },
-    });
-    return res.data.results;    
+      });
+      return res.data.results;
     },
     staleTime: 1000 * 60 * 60,
   });
-    }
+}
